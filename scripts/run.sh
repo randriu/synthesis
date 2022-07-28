0d2080f47dcb4cb5c500908eeab6abdfc4176c37
JulieGysel <julie.gysel@gmail.com>
2022-07-28 21:36:25 +0200

    run

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
-# grid/center maze maze_avoid maze_hexa grid/avoid grid/avoid_2 grid/avoid_3 grid/avoid_4 grid/hexa grid/simple
-# for example in grid/simple grid/avoid grid/avoid_4 grid/center maze maze_long
-for example in maze_long
+for example in grid/avoid grid/center maze maze_long
 do
     props=$(find "workspace/examples/pomdp/$example" -type f -printf "%f\n" | grep prop)
     example="workspace/examples/pomdp/$example"
 
 
-    # timeout 30m python3 paynt/paynt.py \
-    #     --properties $props \
-    #     --project $example ar \
-    #     --pomdp-memory-size 2 \
-    #     --strategy iterative \
-    #     --fsc-synthesis \
+    timeout 30m python3 paynt/paynt.py \
+        --properties $props \
+        --project $example ar \
+        --pomdp-memory-size 2 \
+        --strategy iterative \
+        --fsc-synthesis \
 
-    # timeout 30m python3 paynt/paynt.py \
-    #     --properties $props \
-    #     --project $example ar \
-    #     --pomdp-memory-size 2 \
-    #     --strategy injection \
-    #     --fsc-synthesis \
+    timeout 30m python3 paynt/paynt.py \
+        --properties $props \
+        --project $example ar \
+        --pomdp-memory-size 2 \
+        --strategy injection \
+        --fsc-synthesis \
     
-    python3 paynt/paynt.py \
+    timeout 30m python3 paynt/paynt.py \
         --properties $props \
         --project $example ar \
         --fsc-synthesis \
