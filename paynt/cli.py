47273c3f40dbde6cbb63ab869a49eb3f444bb108
Roman Andriushchenko <iandri@fit.vut.cz>
2022-10-27 14:39:48 +0200

    basic MDP sampling

--- paynt/cli.py
+++ paynt/cli.py
@@ -85,6 +85,8 @@ def setup_logger(log_path = None):
     show_default=True,
     help="counterexample generator",
 )
+@click.option("--sampling", is_flag=True, default=False,
+    help="sample executions")
 @click.option("--profiling", is_flag=True, default=False,
     help="run profiling")
 
@@ -95,6 +97,7 @@ def paynt(
         incomplete_search,
         fsc_synthesis, pomdp_memory_size, fsc_export_result,
         ce_generator,
+        sampling,
         profiling
 ):
     logger.info("This is Paynt version {}.".format(version()))
@@ -116,6 +119,11 @@ def paynt(
     quotient = Sketch.load_sketch(sketch_path, filetype, export,
         properties_path, constants, relative_error)
 
+
+    if sampling:
+        quotient.sample()
+        exit()
+        
     # choose the synthesis method and run the corresponding synthesizer
     if isinstance(quotient, POMDPQuotientContainer) and fsc_synthesis:
         synthesizer = SynthesizerPOMDP(quotient, method)
