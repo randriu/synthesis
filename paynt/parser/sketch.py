b214811c20002307e424127cd2a67d571c4b92eb
Roman Andriushchenko <iandri@fit.vut.cz>
2022-11-10 13:13:48 +0100

    constructing POMDP

--- paynt/parser/sketch.py
+++ paynt/parser/sketch.py
@@ -31,6 +31,7 @@ class Sketch:
         specification = None
         coloring = None
         jani_unfolder = None
+        decpomdp_manager = None
 
         logger.info(f"loading sketch from {sketch_path} ...")
         if filetype == "prism":
@@ -40,10 +41,16 @@ class Sketch:
             specification = PrismParser.parse_specification(properties_path, relative_error)
         else: # filetype == "cassandra"
             decpomdp_manager = stormpy.synthesis.parse_decpomdp(sketch_path)
-            quotient_container = DecPomdpQuotientContainer(decpomdp_manager)
-            return quotient_container
+            logger.info("applying discount factor transformation...")
+            decpomdp_manager.apply_discount_factor_transformation()
+            explicit_quotient = decpomdp_manager.construct_pomdp()
+            optimality = paynt.quotient.property.construct_reward_property(
+                decpomdp_manager.reward_model_name,
+                decpomdp_manager.reward_minimizing,
+                decpomdp_manager.discount_sink_label)
+            specification = paynt.quotient.property.Specification([],optimality)
              
-        assert specification is not None   
+        assert specification is not None
         MarkovChain.initialize(specification)
         
         logger.debug("constructed explicit quotient having {} states and {} actions".format(
@@ -69,16 +76,14 @@ class Sketch:
                 PomdpParser.write_model_in_pomdp_solve_format(explicit_quotient, output_path, property_path)
             exit(0)
 
-        logger.info(f"initializing quotient container...")
         quotient_container = None
         if jani_unfolder is not None:
             quotient_container = DTMCQuotientContainer(explicit_quotient, coloring, specification)
         else:
             assert explicit_quotient.is_nondeterministic_model
-            if explicit_quotient.is_partially_observable:
-                quotient_container = POMDPQuotientContainer(explicit_quotient, specification)
+            if decpomdp_manager is not None and decpomdp_manager.num_agents > 1:
+                quotient_container = DecPomdpQuotientContainer(decpomdp_manager, specification)
             else:
-                # left intentionally blank
-                pass
+                quotient_container = POMDPQuotientContainer(explicit_quotient, specification)
         return quotient_container
 
--- paynt/quotient/quotient_decpomdp.py
+++ paynt/quotient/quotient_decpomdp.py
@@ -5,7 +5,6 @@ import stormpy.pomdp
 from .models import MarkovChain,MDP,DTMC
 from .holes import Hole,Holes,DesignSpace
 from .quotient import QuotientContainer
-from .quotient_pomdp import POMDPQuotientContainer
 
 import paynt
 
@@ -15,25 +14,17 @@ logger = logging.getLogger(__name__)
 
 class DecPomdpQuotientContainer(QuotientContainer):
 
-    def __init__(self, decpomdp_manager):
+    def __init__(self, decpomdp_manager, specification):
+        super().__init__(specification = specification)
+
+        assert self.decpomdp_manager.num_agents > 1
 
         self.decpomdp_manager = decpomdp_manager
-        logger.info(f"dec-POMDP has {decpomdp_manager.num_agents} agents")
+        logger.info(f"dec-POMDP has {self.decpomdp_manager.num_agents} agents")
 
-        # see what happens if you uncomment the line below:
-        # decpomdp_manager.apply_discount_factor_transformation()
-        self.quotient = decpomdp_manager.construct_mdp()
+        self.quotient = self.decpomdp_manager.construct_mdp()
         print("MDP has {} states".format(self.quotient.nr_states))
         print("transitions from state 1: ", self.quotient.transition_matrix.get_row(1))
-
-        # construct specification
-        reward_name = list(self.quotient.reward_models)[0]
-        optimization_direction = "min" if decpomdp_manager.reward_minimizing else "max"
-        formula_str = 'R{"' + reward_name + '"}' + optimization_direction + '=? [F "sink"]'
-        formula = stormpy.parse_properties_without_context(formula_str)[0]
-        optimality = paynt.quotient.property.OptimalityProperty(formula, 0)
-        specification = paynt.quotient.property.Specification([],optimality)
-        MarkovChain.initialize(specification)
 
         logger.debug("nothing to do, aborting...")
         exit()
