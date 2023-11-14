187a2443f1481df8698b4bb9a0210c2919e3f887
Roman Andriushchenko <iandri@fit.vut.cz>
2023-11-14 13:03:48 +0100

    add a choice between deterministic and randomized FSC

--- paynt_pomdp_sketch.py
+++ paynt_pomdp_sketch.py
@@ -16,7 +16,7 @@ def load_sketch(project_path):
     return pomdp_sketch
 
 
-def investigate_hole_assignment(pomdp_sketch, hole_assignment):
+def investigate_hole_assignment(pomdp_sketch, hole_assignment, fsc_is_deterministic):
     print("investigating hole assignment: ", hole_assignment)
     pomdp = pomdp_sketch.build_pomdp(hole_assignment)
     state_is_absorbing = pomdp_sketch.identify_absorbing_states(pomdp)
@@ -24,29 +24,37 @@ def investigate_hole_assignment(pomdp_sketch, hole_assignment):
 
     # return a random k-FSC
     num_nodes = 3
-    fsc = paynt.quotient.pomdp_family.FSC(num_nodes, pomdp.nr_observations)
+    fsc = paynt.quotient.pomdp_family.FSC(num_nodes, pomdp.nr_observations, fsc_is_deterministic)
     # random.seed(42)
     for node in range(num_nodes):
         for obs in range(pomdp.nr_observations):
-            # some random randomized policy
-            available_actions = pomdp_sketch.observation_to_actions[obs]
-            num_samples = min(len(available_actions),node+1)
-            sampled_actions = random.sample(available_actions,num_samples)
-            prob = 1/num_samples
-            randomized_action_selection = {action:prob for action in sampled_actions}
-            fsc.action_function[node][obs] = randomized_action_selection
+            # random deterministic FSC
+            if fsc.is_deterministic:
+                fsc.action_function[node][obs] = random.choice(pomdp_sketch.observation_to_actions[obs])
+            else:
+                # random randomized FSC
+                available_actions = pomdp_sketch.observation_to_actions[obs]
+                num_samples = min(len(available_actions),node+1)
+                sampled_actions = random.sample(available_actions,num_samples)
+                prob = 1/num_samples
+                randomized_action_selection = {action:prob for action in sampled_actions}
+                fsc.action_function[node][obs] = randomized_action_selection
             
             fsc.update_function[node][obs] = random.randrange(num_nodes)
     return fsc
 
 
-
 # enable PAYNT logging
 paynt.cli.setup_logger()
+
+# need to specify beforehand whether FSCs will be deterministic or not
+# since we initialize the FSC unfolder beforehand
+fsc_is_deterministic = False
 
 # load sketch
 project_path="models/pomdp/sketches/obstacles"
 pomdp_sketch = load_sketch(project_path)
+pomdp_sketch.initialize_fsc_unfolder(fsc_is_deterministic)
 print("specification: ", pomdp_sketch.specification)
 print("design space:\n", pomdp_sketch.design_space)
 print("number of holes: ", pomdp_sketch.design_space.num_holes)
@@ -57,7 +65,7 @@ hole_options = [[hole.options[0]] for hole in pomdp_sketch.design_space]
 hole_assignment = pomdp_sketch.design_space.assume_options_copy(hole_options)
 
 # investigate this hole assignment and return an FSC
-fsc = investigate_hole_assignment(pomdp_sketch, hole_assignment)
+fsc = investigate_hole_assignment(pomdp_sketch, hole_assignment, fsc_is_deterministic)
 
 # investigate this FSC and return a hole assignment for which this FSC is violating
 violating_assignments = pomdp_sketch.investigate_fsc(fsc)
