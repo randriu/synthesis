c74fb7f0d4f2fe2e80e18d0c9b1aca1a475e6228
Filip Macak <filip.macak1999@gmail.com>
2024-04-04 17:25:56 +0200

    added some sarsop experiments

--- experiments-beliefs/experiments.py
+++ experiments-beliefs/experiments.py
@@ -127,7 +127,7 @@ def run_sarsop(options, logs_string, experiment_models, timeout):
         model_name = model_name.replace(".", "-")
 
         # THE REST OF THE MODELS
-        command = "./pomdpsol ../models/{} -p 1e-4 -o ../output/{}.out".format(model, model_name)
+        command = "./pomdpsol ../models/{} -p 1e-4 -o ../../../../synthesis-playground/experiments-beliefs/sarsop/output/{}.out".format(model, model_name)
 
         if not overwrite_logs:
             if os.path.isfile(logs_dir.decode("utf-8") + model_name + "/" + "logs.txt"):
@@ -241,10 +241,11 @@ if __name__ == '__main__':
 
     elif "sarsop":
         # experiment_models = ["drone-4-1-80.pomdp", "drone-4-1-95.pomdp", "drone-4-2-80.pomdp", "drone-4-2-95.pomdp", "drone-8-2-80.pomdp", "drone-8-2-95.pomdp", "grid-av-4-01-80.pomdp", "grid-av-4-01-95.pomdp", "refuel-06-80.pomdp", "refuel-06-95.pomdp", "refuel-08-80.pomdp", "refuel-08-95.pomdp", "refuel-20-80.pomdp", "refuel-20-95.pomdp"]
-        experiment_models = ["iff.pomdp", "hallway.pomdp", "learning.c3.pomdp", "learning.c4.pomdp", "milos-aaai97.pomdp", "mit.pomdp", "network.pomdp", "pentagon.pomdp", "query.s3.pomdp", "query.s4.pomdp", "tiger-grid.pomdp"]
+        # experiment_models = ["iff.pomdp", "hallway.pomdp", "learning.c3.pomdp", "learning.c4.pomdp", "milos-aaai97.pomdp", "mit.pomdp", "network.pomdp", "pentagon.pomdp", "query.s3.pomdp", "query.s4.pomdp", "tiger-grid.pomdp"]
+        experiment_models = ["rocks-12-80.pomdp", "rocks-12-95.pomdp", "rocks-12-99.pomdp", "drone-4-1-80.pomdp", "drone-4-1-95.pomdp", "drone-4-1-99.pomdp", "grid-large-30-5-95.pomdp", "grid-large-30-5-80.pomdp", "grid-large-30-5-99.pomdp"]
 
         options = ""
-        logs_string = "sarsop-main"
+        logs_string = "sarsop"
         timeout = 900
         run_sarsop(options, logs_string, experiment_models, timeout)
 
