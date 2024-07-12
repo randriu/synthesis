6a474728918e4ffdcbd045bbc4431b28ac9af675
Antonin Masopust <purple.dragon@email.cz>
2024-07-12 13:37:05 +0200

    fix: add posmg's parent class to binding

--- payntbind/src/synthesis/posmg/bindings.cpp
+++ payntbind/src/synthesis/posmg/bindings.cpp
@@ -1,9 +1,10 @@
 #include "../synthesis.h"
 
 #include "Posmg.h"
+#include "storm/models/sparse/Smg.h"
 
 void bindings_posmg(py::module &m) {
-    py::class_<synthesis::Posmg, std::shared_ptr<synthesis::Posmg>>(m, "Posmg");
+    py::class_<synthesis::Posmg, std::shared_ptr<synthesis::Posmg>, storm::models::sparse::Smg<double>>(m, "Posmg");
     //    .def(py::init<storm::storage::sparse::ModelComponents<double> const&>(), py::arg("components"))
     //    .def(py::init<storm::storage::sparse::ModelComponents<double> &&>(), py::arg("components"));
 
