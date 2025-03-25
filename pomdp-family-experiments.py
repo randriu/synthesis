0e3e9e756a4edc390e749d5b178fd8274a47b432
Antonin Masopust <purple.dragon@email.cz>
2025-03-25 21:11:51 +0100

    tweak experiments script

--- pomdp-family-experiments.py
+++ pomdp-family-experiments.py
@@ -6,7 +6,24 @@ import sys
 onebyone = "--onebyone" in sys.argv
 
 # List of models
-posmg_model = ["obstacles-maze-4", "obstacles-maze-4-2", "obstacles-maze-4-3", "refuel-04", "refuel-04-slip", "rover-100", "dpm-switch-q10"]
+# pomdp_family_model = ["obstacles-maze-4", "obstacles-maze-4-2", "obstacles-maze-4-3", "refuel-04", "refuel-04-slip", "rover-100", "dpm-switch-q10"]
+# pomdp_family_model = [
+#     "avoid-3x5-1o-noslip-down",
+#     "avoid-3x5-1o-noslip-down --fsc-memory-size 2",
+#     "avoid-3x5-1o-slip-down",
+#     "avoid-3x5-1o-slip-nodown",
+#     "avoid-3x5-1o-slip-nodown --fsc-memory-size 2",
+#     "avoid-4x4-3o-slip-1detect-nodown-lr",
+#     "avoid-4x5-2o-noslip-1crash-nodown-lr",
+#     "avoid-4x5-2o-noslip-1detect-nodown-lr",
+#     "avoid-4x5-2o-slip-1detect-down",
+#     "avoid-4x5-2o-slip-1detect-down-lr",
+#     "avoid-4x5-2o-slip-1detect-nodown-lr",
+#     "avoid-5x4-3o-slip-1detect-nodown-lr",
+
+#     "avoid-5x4-2o-slip-1detect-nodown-lr"
+# ]
+pomdp_family_model = ["debugging-2"]
 
 # Create experiments folder if it doesn't exist
 experiments_dir = "experiments"
@@ -24,7 +41,7 @@ if not os.path.exists(timestamped_dir):
     os.makedirs(timestamped_dir)
 
 # Iterate over the list and run the command
-for model in posmg_model:
+for model in pomdp_family_model:
     log_file = os.path.join(timestamped_dir, f"{model}.log")
 
     # Skip the model if log_file already exists and --override is not used
@@ -32,7 +49,7 @@ for model in posmg_model:
         print(f"Skipping {model}, log file already exists.")
         continue
 
-    command = f"gtimeout 3600 python3 paynt.py models/pomdp/sketches/{model}"
+    command = f"gtimeout 1500 python3 paynt.py models/pomdp/sketches/{model}"
     if onebyone:
         command += " --method onebyone"
 
