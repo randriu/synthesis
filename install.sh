7c77c6e62dd5afc27b24cbd38e29ea4b544f2c15
Ladislav Dokoupil <ldokoupi@redhat.com>
2025-05-07 12:23:51 +0200

    update install guide

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ COMPILE_JOBS=$(nproc)
 
 # environment variables
 PAYNT_ROOT=`pwd`
-PREREQUISITES=${PAYNT_ROOT}/prerequisites # modify this to install prerequisites outside of Paynt
+PREREQUISITES=${PAYNT_ROOT}/prerequisites
 
 # storm and stormpy dependencies
 sudo apt update -qq
@@ -17,46 +17,47 @@ sudo apt install -y maven uuid-dev python3-dev python3-venv python3-pip
 # prerequisites
 mkdir -p ${PREREQUISITES}
 
-# build cvc5 (optional)
-# cd ${PREREQUISITES}
-# git clone --depth 1 --branch cvc5-1.0.0 https://github.com/cvc5/cvc5.git cvc5
-# cd ${PREREQUISITES}/cvc5
-# source ${PAYNT_ROOT}/env/bin/activate
-# ./configure.sh --prefix="." --auto-download --python-bindings
-# cd build
-# make --jobs ${COMPILE_JOBS}
-# make install
-# deactivate
-
 # build storm
 cd ${PREREQUISITES}
-git clone https://github.com/moves-rwth/storm.git storm
-# git clone --branch stable https://github.com/moves-rwth/storm.git storm
+git clone https://github.com/moves-rwth/storm.git storm -b 1.9.0
 mkdir -p ${PREREQUISITES}/storm/build
 cd ${PREREQUISITES}/storm/build
 cmake ..
-make storm storm-cli storm-pomdp --jobs ${COMPILE_JOBS}
-# make check --jobs ${COMPILE_JOBS}
+make --jobs ${COMPILE_JOBS}
+
+export STORM_DIR=${PREREQUISITES}/storm
+export PATH=$PATH:$STORM_DIR/build/bin
 
 # setup and activate python environment
 python3 -m venv ${PREREQUISITES}/venv
 source ${PREREQUISITES}/venv/bin/activate
 pip3 install wheel
 
+# build carl
+cd ${PREREQUISITES}
+git clone https://github.com/moves-rwth/carl-storm carl
+mkdir -p ${PREREQUISITES}/carl/build
+cd ${PREREQUISITES}/carl/build
+cmake ..
+make lib_carl
+
 # build pycarl
 cd ${PREREQUISITES}
-git clone https://github.com/moves-rwth/pycarl.git pycarl
+git clone https://github.com/moves-rwth/pycarl.git pycarl -b 2.3.0
 cd ${PREREQUISITES}/pycarl
 python3 setup.py develop
-#[TEST] python3 setup.py test
 
 # build stormpy
 cd ${PREREQUISITES}
-git clone https://github.com/moves-rwth/stormpy.git stormpy
-# git clone --branch stable https://github.com/moves-rwth/stormpy.git stormpy
+git clone https://github.com/moves-rwth/stormpy.git stormpy -b 1.9.0
 cd ${PREREQUISITES}/stormpy
 python3 setup.py develop
-# python3 setup.py test
+
+# build dtcontrol
+cd ${PREREQUISITES}
+git clone https://gitlab.com/live-lab/software/dtcontrol.git dtcontrol -b paynt-colab
+cd ${PREREQUISITES}/dtcontrol
+pip install .
 
 # paynt dependencies
 sudo apt -y install graphviz
