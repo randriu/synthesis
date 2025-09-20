a5410794e43d2e7ff97e682672d9f11b0ed72dea
Filip Macak <filip.macak1999@gmail.com>
2025-09-20 13:26:10 +0200

    testing linux wheel build

--- .github/workflows/before-all-linux.sh
+++ .github/workflows/before-all-linux.sh
@@ -8,6 +8,32 @@ dnf install -y boost-devel cln-devel gmp-devel glpk-devel hwloc-devel z3-devel x
 
 cd /tmp
 
+# Install Python 3.10 from source
+PYTHON_VERSION=3.10.18
+NR_JOBS=$(nproc)
+
+curl -fsSLO https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tgz
+tar -xzf Python-${PYTHON_VERSION}.tgz
+cd Python-${PYTHON_VERSION}
+
+# Install build dependencies
+dnf install -y gcc make openssl-devel bzip2-devel libffi-devel zlib-devel readline-devel sqlite-devel tk-devel xz-devel
+
+./configure --enable-optimizations --with-ensurepip=install
+make -j ${NR_JOBS}
+make altinstall
+
+cd ..
+
+# Install python3.10-devel and python3.10-venv
+PYTHON3_10_BIN=/usr/local/bin/python3.10
+
+${PYTHON3_10_BIN} -m ensurepip
+${PYTHON3_10_BIN} -m pip install --upgrade pip
+${PYTHON3_10_BIN} -m pip install venv
+
+# python3.10-devel is provided by headers and config files installed by make altinstall
+
 # Install ginac
 # curl -fsSLO https://www.ginac.de/ginac-${ginac_version}.tar.bz2
 # tar -jxf ginac-${ginac_version}.tar.bz2
