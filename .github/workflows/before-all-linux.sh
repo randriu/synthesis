0f3a6f451e007eab003d6c73ef5e98add8b875aa
Filip Macak <filip.macak1999@gmail.com>
2025-09-23 17:35:17 +0200

    updating the wheel build workflows

--- .github/workflows/before-all-linux.sh
+++ .github/workflows/before-all-linux.sh
@@ -4,13 +4,12 @@
 
 set -e -u
 
-ginac_version=1.8.9
-
 dnf install -y boost-devel cln-devel gmp-devel glpk-devel hwloc-devel z3-devel xerces-c-devel eigen3-devel python3-devel # missing ginac
 
 cd /tmp
 
 # Install ginac
+ginac_version=1.8.9
 curl -fsSLO https://www.ginac.de/ginac-${ginac_version}.tar.bz2
 tar -jxf ginac-${ginac_version}.tar.bz2
 cd ginac-${ginac_version}
@@ -19,13 +18,12 @@ make -j ${NR_JOBS}
 make install
 cd ..
 
-# Install Storm
-git clone https://github.com/moves-rwth/storm.git -b ${STORM_VERSION}
-cd storm
-mkdir build
-cd build
-cmake .. -DSTORM_BUILD_TESTS=OFF -DSTORM_BUILD_EXECUTABLES=OFF -DSTORM_PORTABLE=ON
-make -j ${NR_JOBS}
-make install
+# Install Boost 1.83 from source
+BOOST_VERSION=1.83.0
+BOOST_VERSION_UNDERSCORE=${BOOST_VERSION//./_}
+curl -fsSLO https://archives.boost.io/release/${BOOST_VERSION}/source/boost_${BOOST_VERSION_UNDERSCORE}.tar.gz
+tar -xzf boost_${BOOST_VERSION_UNDERSCORE}.tar.gz
+cd boost_${BOOST_VERSION_UNDERSCORE}
+./bootstrap.sh --prefix=/usr/local
+./b2 install -j ${NR_JOBS}
 cd ..
-rm -rf build
